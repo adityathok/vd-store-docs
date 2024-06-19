87283a3ad959d09c47832580347e8c6b392f699a
adityathok <aditya0thok@gmail.com>
2024-06-19 10:37:57 +0700

    update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://adityathok.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/vd-store-docs/',
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
 
